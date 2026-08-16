e747498888c4637eb73b1a4dd8b99d096f9075e0
Gabriel Soto <gabrielsotocastro@gmail.com>
2026-08-16 12:45:23 -0400

    Sincronización de rutas core de producción y apertura global de CORS

--- src/pages/home.js
+++ src/pages/home.js
@@ -425,7 +425,7 @@ Home.renderMasterHeader = function() {
                        // ⏱️ PARCHE DE DEBOUNCE SANEADO: Consulta a Django sin romper el layout visual
             temporizadorBusqueda = setTimeout(() => {
                 // 🎯 CONTROL ABSOLUTO: Inyectamos la URL rígida integral hacia Django
-                let urlApi = `http://127.0.0.1:8000/api/v1/buscador/?q=${encodeURIComponent(termino.trim())}`;
+                let urlApi = `https://apio-ecomerce-sotfware-b2b-sotosystem.onrender.com/api/v1/buscador/?q=${encodeURIComponent(termino.trim())}`;
                 
                 console.log(`📡 [Buscador Apio POS]: Consultando backend local -> ${urlApi}`);
 
